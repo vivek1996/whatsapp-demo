ff2bddfbe81b3343fef3918c919c27745355a88b
Jyoti <jyothi@mobifyi.com>
2018-10-09 11:13:26 +0530

    chat list commit

--- src/app/chat-items/chat-items.component.ts
+++ src/app/chat-items/chat-items.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatItemsComponent implements OnInit {
   userAvatar = 'https://via.placeholder.com/35x35';
-  constructor() { }
+  details: Array<object> = [
+    {
+      username: 'tim',
+      message: 'logygvcjhbzsjcb',
+      time: '10:35pm',
+      unreadCount: 2
+    },
+    {
+      username: 'rfcy',
+      message: 'dfnvjjnbfksjncb',
+      time: '02:35pm',
+      unreadCount: 2
+    },
+    {
+      username: 'tevrf',
+      message: 'logygvcjhbzsjcbfrvj',
+      time: '10:35am',
+      unreadCount: undefined
+    },
+    {
+      username: 'yui',
+      message: 'lbjfd cjsjcb',
+      time: '7:03pm',
+      unreadCount: 7
+    },
+    {
+      username: 'mossd',
+      message: 'logygvcjhbzsfv',
+      time: '01:40am',
+      unreadCount: 4
+    }
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
